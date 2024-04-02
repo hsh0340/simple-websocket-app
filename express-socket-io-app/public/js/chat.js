c84f63bb6e219e14a52096fc80410e1a0989969a
hsh0340 <hsh0340@naver.com>
2024-04-02 16:38:24 +0900

    feat: client join 이벤트 구현

--- express-socket-io-app/public/js/chat.js
+++ express-socket-io-app/public/js/chat.js
@@ -1,3 +1,12 @@
 const socket = io();
 
-console.log(socket);
+const query = new URLSearchParams(location.search);
+const username = query.get('username');
+const room = query.get('room');
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error);
+        location.href = '/';
+    }
+})
