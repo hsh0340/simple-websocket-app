97391f1a1245a52ddcfa08e84ebd12684d85de93
hsh0340 <hsh0340@naver.com>
2024-04-02 16:19:44 +0900

    feat: socket io 연결

--- express-socket-io-app/src/index.js
+++ express-socket-io-app/src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+
+const http = require('http');
+const { Server } = require('socket.io');
+// app 객체를 인자로 전달
+const server = http.createServer(app);
+// server 객체를 인자로 전달
+const io = new Server(server);
+
+const path = require('path');
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 const PORT = 8000;
-app.listen(PORT, () => {
+
+io.on('connection', () => {
+    console.log(socket);
+    socket.on('join', () => {});
+    socket.on('sendMessage', () => {});
+    socket.on('disconnect', () => {});
+})
+
+server.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`);
 })
