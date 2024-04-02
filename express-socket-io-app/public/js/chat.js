09cb98fca72babee85c0c11c7d12b1fcd76a33c0
hsh0340 <hsh0340@naver.com>
2024-04-02 18:20:01 +0900

    feat: roomData 이벤트 on 동작 구현

--- express-socket-io-app/public/js/chat.js
+++ express-socket-io-app/public/js/chat.js
@@ -3,10 +3,20 @@ const socket = io();
 const query = new URLSearchParams(location.search);
 const username = query.get('username');
 const room = query.get('room');
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 socket.emit('join', { username, room }, (error) => {
     if (error) {
         alert(error);
         location.href = '/';
     }
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users,
+    })
+
+    document.querySelector('#sidebar').innerHTML = html;
 })
